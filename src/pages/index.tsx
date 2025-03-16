d839bcf67eeadc6fdfaf985108512743d49c9991
Fonzy57 <traben@hotmail.fr>
2025-03-16 09:31:27 +0100

    Change home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
 /* import { Geist, Geist_Mono } from "next/font/google"; */
 
 // COMPONENTS
-import { CustomButton } from "@/components/button/button";
-import { CustomIcon } from "@/components/icons/icon";
-
-// TYPING & ENUM
-import { IconName } from "@/components/icons/icon.enum";
-import { ButtonColor, ButtonSize, ButtonWidth } from "@/components/button/button.enum";
+import { LinkButton } from "@/components/button/link-button";
 
 /* const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,136 +18,8 @@ export default function Home() {
     <div className="pt-10 px-8">
       <h1 className="text-3xl font-bold underline text-center">Bienvenue sur le site OnlyFlan</h1>
       <h2 className="text-center mt-4">Cette fois, vous n’allez pas être mou du flan</h2>
-
-      <h3 className="text-2xl">En dessous les composants déjà créés 👇</h3>
-
-      <h4 className="mt-10 text-2xl underline">Composant CustomIcon :</h4>
-      <div className="flex items-center gap-3 mt-3">
-        <CustomIcon name={IconName.ArrowDown} size={32} onClick={() => console.log("je clique")} />
-        <CustomIcon name={IconName.At} size={32} />
-        <CustomIcon name={IconName.Envelope} size={32} />
-        <CustomIcon name={IconName.Eye} size={32} className="text-main" />
-        <CustomIcon name={IconName.EyeClose} size={32} />
-        <CustomIcon name={IconName.Facebook} size={32} />
-        <CustomIcon name={IconName.Instagram} size={32} />
-        <CustomIcon name={IconName.Location} size={32} />
-      </div>
-
-      <h4 className="mt-10 text-2xl underline">Composant CustomButton :</h4>
-      <div className="mt-2">
-        <h5 className="text-2xl">Button Primary normal :</h5>
-        <div className="flex items-center gap-3 mt-3">
-          <div className="flex flex-col gap-2">
-            <p>Default :</p>
-            <CustomButton>Button</CustomButton>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p>With icon :</p>
-            <CustomButton iconName={IconName.Envelope}>Button</CustomButton>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p>Disabled :</p>
-            <CustomButton disabled={true}>Button</CustomButton>
-          </div>
-          <div className="flex flex-col gap-2 w-full">
-            <p>Full width :</p>
-            <CustomButton width={ButtonWidth.Full}>Button</CustomButton>
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-2">
-        <h5 className="text-2xl">Button Primary tiny :</h5>
-        <div className="flex items-center gap-3 mt-3">
-          <div className="flex flex-col gap-2">
-            <p>Default :</p>
-            <CustomButton size={ButtonSize.Tiny}>Button</CustomButton>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p>With icon :</p>
-            <CustomButton size={ButtonSize.Tiny} iconName={IconName.Envelope}>
-              Button
-            </CustomButton>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p>Disabled :</p>
-            <CustomButton size={ButtonSize.Tiny} disabled={true}>
-              Button
-            </CustomButton>
-          </div>
-          <div className="flex flex-col gap-2 w-full">
-            <p>Full width :</p>
-            <CustomButton size={ButtonSize.Tiny} width={ButtonWidth.Full}>
-              Button
-            </CustomButton>
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-2">
-        <h5 className="text-2xl">Button Secondary normal :</h5>
-        <div className="flex items-center gap-3 mt-3">
-          <div className="flex flex-col gap-2">
-            <p>Default :</p>
-            <CustomButton color={ButtonColor.Secondary}>Button</CustomButton>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p>With icon :</p>
-            <CustomButton color={ButtonColor.Secondary} iconName={IconName.Envelope}>
-              Button
-            </CustomButton>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p>Disabled :</p>
-            <CustomButton color={ButtonColor.Secondary} disabled={true}>
-              Button
-            </CustomButton>
-          </div>
-          <div className="flex flex-col gap-2 w-full">
-            <p>Full width :</p>
-            <CustomButton color={ButtonColor.Secondary} width={ButtonWidth.Full}>
-              Button
-            </CustomButton>
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-2">
-        <h5 className="text-2xl">Button Secondary tiny :</h5>
-        <div className="flex items-center gap-3 mt-3">
-          <div className="flex flex-col gap-2">
-            <p>Default :</p>
-            <CustomButton color={ButtonColor.Secondary} size={ButtonSize.Tiny}>
-              Button
-            </CustomButton>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p>With icon :</p>
-            <CustomButton
-              color={ButtonColor.Secondary}
-              size={ButtonSize.Tiny}
-              iconName={IconName.Envelope}
-            >
-              Button
-            </CustomButton>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p>Disabled :</p>
-            <CustomButton color={ButtonColor.Secondary} size={ButtonSize.Tiny} disabled={true}>
-              Button
-            </CustomButton>
-          </div>
-          <div className="flex flex-col gap-2 w-full">
-            <p>Full width :</p>
-            <CustomButton
-              color={ButtonColor.Secondary}
-              size={ButtonSize.Tiny}
-              width={ButtonWidth.Full}
-            >
-              Button
-            </CustomButton>
-          </div>
-        </div>
+      <div className="mx-auto w-max mt-10">
+        <LinkButton url="/components">Voir les composants</LinkButton>
       </div>
     </div>
   );
